d7c8d35e4325976c35751ad7527a0783fca65133
thetubeguy <59772776+thetubeguy@users.noreply.github.com>
2022-07-22 11:31:17 +0100

    Added deserialisation of Json file into list of Flight objects. Edited csproj file to copy Json Data into build folder. Added test for more than one local airport.

--- OTBHolidaySearch.Test/DeserialisationTests/DeserialisationTests.cs
+++ OTBHolidaySearch.Test/DeserialisationTests/DeserialisationTests.cs
@@ -4,14 +4,20 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using FluentAssertions;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace OTBHolidaySearch.Test.DeserialisationTests
 {
     public class Tests
     {
+        public string FlightFilename = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) + "\\FlightData.json";
+
         [SetUp]
         public void Setup()
         {
+           
         }
 
         [Test]
@@ -56,7 +62,7 @@ namespace OTBHolidaySearch.Test.DeserialisationTests
                                     ""name"": ""Sol Katmandu Park & Resort"",
                                     ""arrival_date"": ""2023-06-15"",
                                     ""price_per_night"": 59,
-                                    ""local_airports"": [""PMI""],
+                                    ""local_airports"": [""PMI"",""ABC""],
                                     ""nights"": 14
             }";
 
@@ -74,7 +80,42 @@ namespace OTBHolidaySearch.Test.DeserialisationTests
             hotel?.ArrivalDate.Should().BeSameDateAs(arrivalDate);
             hotel?.PricePerNight.Should().Be(59);
             hotel?.LocalAirports?[0].Should().Be("PMI");
+            hotel?.LocalAirports?[1].Should().Be("ABC");
             hotel?.Nights.Should().Be(14);
+
+        }
+
+        [Test]
+        public void Deserialise_A_Json_String_From_A_File_Into_A_List_Of_Flight_Objects()
+        {
+            // Arrange
+
+            string jsonString = File.ReadAllText(FlightFilename);
+
+            DateTime departureDate1 = new(2023, 07, 01);
+            DateTime departureDate11 = new(2023, 10, 25);
+
+            // Act
+
+            Flights? flights = JsonSerializer.Deserialize<Flights>(jsonString);
+
+
+            // Assert
+
+            flights?.FlightList?[0]?.Id.Should().Be(1);
+            flights?.FlightList?[0]?.Airline.Should().Be("First Class Air");
+            flights?.FlightList?[0]?.From.Should().Be("MAN");
+            flights?.FlightList?[0]?.To.Should().Be("TFS");
+            flights?.FlightList?[0]?.Price.Should().Be(470);
+            flights?.FlightList?[0]?.DepartureDate.Should().BeSameDateAs(departureDate1);
+
+            flights?.FlightList?[11]?.Id.Should().Be(12);
+            flights?.FlightList?[11]?.Airline.Should().Be("Trans American Airlines");
+            flights?.FlightList?[11]?.From.Should().Be("MAN");
+            flights?.FlightList?[11]?.To.Should().Be("AGP");
+            flights?.FlightList?[11]?.Price.Should().Be(202);
+            flights?.FlightList?[11]?.DepartureDate.Should().BeSameDateAs(departureDate11);
+
 
         }
     }
