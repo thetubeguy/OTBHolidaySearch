36e94a8f47be08a6b24b47553fa4476ac8298764
thetubeguy <59772776+thetubeguy@users.noreply.github.com>
2022-07-22 13:55:40 +0100

    Added HolidaySearch class and test to check critera can be passed as a structure to its constructor.

---
+++ OTBHolidaySearch.Test/SearchTests/SearchTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using OTBHolidaySearch;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace OTBHolidaySearch.Test.SearchTests
+{
+    public class SearchTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+
+        public void Check_Can_Pass_Criteria_Structure_To_HolidaySearch()
+        {
+            // Arrange
+
+            HolidaySearch holidaySearch = new(new HolidaySearch.Criteria
+            {
+                DepartingFrom = "MAN",
+                TravellingTo =  "AGP",
+                DepartureDate =  new DateTime(2023,07,01),
+                Duration = 7
+            });
+
+
+            // Act
+
+
+
+            // Assert
+
+            holidaySearch.holidayCriteria.DepartingFrom.Should().Be("MAN");
+            holidaySearch.holidayCriteria.TravellingTo.Should().Be("AGP");
+            holidaySearch.holidayCriteria.DepartureDate.Should().BeSameDateAs(new DateTime(2023, 07, 01));
+            holidaySearch.holidayCriteria.Duration.Should().Be(7);
+        }
+
+    }
+}
---
+++ OTBHolidaySearch/HolidaySearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTBHolidaySearch
+{
+    public class HolidaySearch
+    {
+        public struct Criteria
+        {
+            public string DepartingFrom;
+            public string TravellingTo;
+            public DateTime DepartureDate;
+            public int Duration;
+        }
+
+        public Criteria holidayCriteria { get; private set; } 
+        public HolidaySearch(Criteria criteria)
+        {
+            this.holidayCriteria = criteria;
+        }
+    }
+}
